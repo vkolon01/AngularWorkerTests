8dddc2d2ff21cfae490b2ba92eb270d8e859f0b4
vitali.kolontko <vitali.kolontko@ultra-as.com>
2020-03-06 14:33:30 +0000

    sdfg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestcomponentComponent } from './testcomponent/testcomponent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestcomponentComponent
   ],
   imports: [
     BrowserModule,
